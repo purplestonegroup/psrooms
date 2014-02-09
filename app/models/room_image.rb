e692c0e2573aa10cd97deb4aa6b986a8596c7281
Silvio Vaz <hello@sevenvisualsigns.com>
2014-02-09 11:53:40 +0000

    adding the paperclip update line to the app model

--- app/models/room_image.rb
+++ app/models/room_image.rb
@@ -4,4 +4,6 @@ class RoomImage < ActiveRecord::Base
 	
 	has_attached_file :image, styles: { large: "700x450#", medium: "250x150#" }
 
+	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+
 end
