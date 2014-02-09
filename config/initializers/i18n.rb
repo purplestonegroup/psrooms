cf32b824d27a6598e29b2a91873851a208bca3c9
Silvio Vaz <hello@sevenvisualsigns.com>
2014-02-09 15:45:56 +0000

    setting the first config of languages

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,5 +3,10 @@ I18n.default_locale = :en
 
 LANGUAGES = [
 	['English', 'en'],
-	["Espa&ntilde;ol".html_safe, 'es']
+	["Espa&ntilde;ol".html_safe, 'es'],
+	["Fran&ccedil;ais".html_safe, 'fr'], 
+	["Rom&acirc;n&#x103;".html_safe, 'rm'],
+	["italiano".html_safe, 'it'],
+	["portugu&ecirc;s".html_safe, 'pt'],
+	["Deutsch".html_safe, 'de']
 ]
